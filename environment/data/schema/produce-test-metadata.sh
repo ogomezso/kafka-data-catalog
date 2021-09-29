4878f85ef83631d7e4b64595ec290c0742419534
Oscar Gomez <ogomezso0@gmail.com>
2021-09-29 16:10:51 +0200

    synthetic data generation & project unnecesary files clean up

--- environment/data/schema/produce-test-metadata.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-cat metadata-test-dataset.json | kafka-avro-console-producer --topic metadata_topic --bootstrap-server broker:29092 --property schema.registry.url=http://schema-registry:8081 --property key.schema='{"type":"string"}' --property value.schema="$(< topic_metadata.avsc)" --property parse.key=true --property key.separator=":"
